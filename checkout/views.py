a0a33f1cf5a28e46cbd6017f06687910954d8d01
Charlie Watson <charliewatson87@gmail.com>
2022-04-23 11:56:34 +0000

    Added order summary to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -114,7 +114,7 @@ def checkout_success(request, order_number):
     
     template = 'checkout/checkout_success.html'
     context = {
-        order: order,
+        'order': order,
     }
 
     return render(request, template, context)
